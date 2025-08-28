61f56ac7ab6232b11f0a2d0d14638a767b1f4eac
MihajloCumic <mihajlo.cumic1999@gmail.com>
2025-08-28 17:29:45 +0200

    added docs

---
+++ src/main/java/com/example/product_service/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.example.product_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info().title("Product Service"))
+                .schemaRequirement("bearerAuth", new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+
+}
--- src/main/java/com/example/product_service/config/SecurityConfig.java
+++ src/main/java/com/example/product_service/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated());
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
